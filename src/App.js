7b207d1cffce1b883a25a4ae3d0245f7004447d9
Ricardo Fabio Caterino Polini <rcaterino.dev@gmail.com>
2022-08-03 07:12:03 +0200

    Aprendiendo React Context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContext = React.createContext({
+  //El contexto es como un manejador de estado, que se utiliza, entre otras cosas, para compartir propiedades entre componentes, sin importar su nivel de dependencia en relación a otros componentes. Esto permite.
+  //Este es un componente proveedor o y sus propiedades serán utilizadas por uno o más componentes consumidores.
+
+  usuarios: [],
+  titulo: 'default',
+});
+
+//el siguiente componente de nombre Lista, es el componente consumidor del contexto que provee el componente padre (el provider)
+class Lista extends React.Component {
+  render() {
+    return(
+      <AppContext.Consumer> 
+        {value => /*esta función permite accesar a todas las propiedades del contexto*/ 
+          {
+            return(
+              <React.Fragment>
+                <h1>{value.titulo}</h1>
+                <ul>
+                  {value.usuarios.map(usuario =>
+                    <li>{usuario}</li>
+                    )}
+                </ul>
+              </React.Fragment>
+            );
+          }
+        }
+      </AppContext.Consumer>//Consumer declara al consumidor del contexto
+    )
+  }
+}
+
+//Lo siguiente es el componente padre que declara un proveedor/provider llamado Lista
+class App extends React.Component {
+  render() {
+    return (
+      <AppContext.Provider value = {{
+        usuarios: ['leo', 'juan'],
+        titulo: 'App',
+      }}>
+        <Lista />
+      </AppContext.Provider>
+    );
+  }
 }
 
 export default App;
